d6b66a6c9c969242f1d52f89ddffb629a49defc6
renzien <alifrizki1011@gmail.com>
2021-10-21 16:03:20 +0700

    add modul3

---
+++ src/modul3/latihan/latihan.java
@@ -0,0 +1,4 @@
+package modul3.latihan;
+
+public class latihan {
+}
---
+++ src/modul3/percobaan/Alamat_Rumah.java
@@ -0,0 +1,9 @@
+package modul3.percobaan;
+
+public class Alamat_Rumah {
+    String alamat_rumah;
+
+    public void setAlamat(String x) {
+        alamat_rumah = x;
+    }
+}
